6ca26b05af47d4aa6e39039ceeda440209670bd7
Jessica Dembe <jmdembe@gmail.com>
2016-12-14 14:50:00 -0500

    Able to parse data file

--- parse-read.js
+++ parse-read.js
@@ -1,14 +1,15 @@
 var fs = require('fs');
 var parse = require('csv-parse');
 
+var theFileToRead = 'dc-parking-data-node/simple_data/parking_feb_2016.csv';
 
-var theFileToRead = null;
-
-var data = fs.readFile('dc-parking-data-node/simple_data/parking_feb_2016.csv', function processFile(err, parsed) {
-    parsed.toString(parsed);
-    console.log(parsed);
-
-
+var parsedData = fs.readFile(theFileToRead, function processFile(err, data) {
+  var stringifiedData = data.toString('utf-8');
+  parse(stringifiedData, function parseData(x,y) {
+    console.log(y);
+  });
 });
+
+// data = parse(data, [delimiter]);
 
 // var violationList = parse()
