ad54256e401f954776dbac51576630ad49d61394
Jessica Dembe <jmdembe@gmail.com>
2016-12-14 22:09:32 -0500

    Moved function into the one that is on the namespace

--- parse-read.js
+++ parse-read.js
@@ -7,45 +7,47 @@ module.require = {
     processFile: processFile
 };
 
-fs.readFile(theFileToRead, function processFile(err, data){
-  if(err) {
-    console.error(err);
-    return;
-  }
 
-  var stringifiedData = data.toString('utf-8');
+function processFile(call) {
 
-  parse(stringifiedData, function parseData(err, data) {
+  fs.readFile(theFileToRead, function processFile(err, data){
     if(err) {
       console.error(err);
       return;
     }
-    else {
-      console.log("Level 1 Answer 1: ", (data.length)-1);
 
-      var result = data.map(function violations(citation){
-        return citation[9];
+    var stringifiedData = data.toString('utf-8');
 
-      });
-      result.shift();
-      result.forEach(function count(citationType) {
-        if(violationType[citationType] === undefined) {
-            violationType[citationType] = 1;
-        } else {
-          violationType[citationType]++;
-        }
-      });
-      console.log("Level 1 Answer 2: ", violationType);
-      var sortedViolation = Object.keys(violationType).sort(function (a, b) {
-        if (violationType[a] < violationType[b]) {
-          return 1;
-        } else {
-          return -1;
-        }
-      });
-      console.log( sortedViolation[0], violationType[sortedViolation[0]] );
-    }
+    parse(stringifiedData, function parseData(err, data) {
+      if(err) {
+        console.error(err);
+        return;
+      }
+      else {
+        console.log("Level 1 Answer 1: ", (data.length)-1);
+
+        var result = data.map(function violations(citation){
+          return citation[9];
+
+        });
+        result.shift();
+        result.forEach(function count(citationType) {
+          if(violationType[citationType] === undefined) {
+              violationType[citationType] = 1;
+          } else {
+            violationType[citationType]++;
+          }
+        });
+        console.log("Level 1 Answer 2: ", violationType);
+        var sortedViolation = Object.keys(violationType).sort(function (a, b) {
+          if (violationType[a] < violationType[b]) {
+            return 1;
+          } else {
+            return -1;
+          }
+        });
+        console.log( sortedViolation[0], violationType[sortedViolation[0]] );
+      }
+    });
   });
-
-
-});
+}
